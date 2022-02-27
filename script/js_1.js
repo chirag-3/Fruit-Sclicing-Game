0ee39ab8a072fb74b1d551f4e937b264d91e3d01
Chirag Lala <chirag42001@gmail.com>
2022-02-27 20:46:05 +0530

    game almost complete

---
+++ script/js_1.js
@@ -0,0 +1,62 @@
+$(function(){
+  var life = 4;
+  var score = 0;
+  $("#start-reset").button();
+  $("#start-reset").text("START");
+
+  $("#start-reset").click(function(){
+    s = $(this).text();
+    if(s=="RESET")
+      location.reload();
+    var fx = Math.floor(Math.random()*$("#play-area").width()-85);
+    if(fx<0)
+      fx = 0;
+    var fy =  $("#play-area").height()+10;  
+    var r = setInterval(function(){
+      if(fy>$("#play-area").height())
+      {
+        window.console.log("once look here \n");
+        $("#l"+life).removeClass("ui-icon ui-icon-heart");
+        life--;
+        if(life==0)
+        {
+          endGame();
+        }
+        fx =  Math.floor(Math.random()*$("#play-area").width()-85);
+        // fx=0;
+        if(fx<0)
+          fx = 0;
+        fy = -62;
+        // fy = 0;
+        $("#img1").attr({"src":"fruit/fruit"+(Math.floor(Math.random()*7)+1)+".png"});
+        $("#img1").css({"left":fx,"top":fy});
+        // window.console.log("calling show"+$("#img1").show());
+        // window.console.log("deciding ("+fx+","+fy+")");
+      }
+      $("#img1").show();
+      fy = fy+10;
+      // window.console.log("moving ("+fx+","+fy+")");
+      $("img").css("top",fy);  
+  },100)
+  $("#img1").click(function(){
+    score++;
+    $("#score span").text(score);
+    $("#img1").hide("explode",50);
+    fy = $("#play-area").height()+10;
+    life++;
+  });
+  function endGame(){
+    clearInterval(r);
+    $("#play-area").html(`
+       <div id="final-score">
+        GAME OVER<br>    
+       SCORE : `+score+`</div>`
+    );
+  }
+
+
+  });
+
+
+ 
+});
