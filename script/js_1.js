cac33122de02b3f507263f13fea24e7ee3a54481
Chirag Lala <chirag42001@gmail.com>
2022-03-01 16:14:38 +0530

    Game made more challenging by increasing speed as score increase

--- script/js_1.js
+++ script/js_1.js
@@ -38,11 +38,30 @@ $(function(){
   });
 
   function begin(){
+    var move = 1;
+    if(score<=10)
+      move = move + 5;
+    else if(score<=20)
+      move = move + 7;
+    else if(score<=40)
+      move = move + 12;
+    else if(score<=50)
+      move = move + 15;
+    else if(score<=60)
+      move = move + 17;
+    else if(score<=70)
+      move = move + 19;
+    else if(score<=80)
+      move = move + 23;
+    else
+      move = move + 27;  
+    
+
     fy =  $("#play-area").height()+10;
     r = setInterval(function(){
       if(fy>$("#play-area").height())
       {
-        window.console.log("once look here \n");
+        // window.console.log("once look here \n");
         $("#l"+life).removeClass("ui-icon ui-icon-heart");
         life--;
         if(life==0)
@@ -50,7 +69,7 @@ $(function(){
           endGame();
         }
         fx =  Math.random()*($("#play-area").width()-85);
-        window.console.log(" new x coordinate generated "+fx);
+        // window.console.log(" new x coordinate generated "+fx);
         if(fx<0)
           fx = 0;
         fy = -62;
@@ -59,7 +78,7 @@ $(function(){
         
       }
       $("#img1").show();
-      fy = fy+10;   
+      fy = fy+move;   
       $("#img1").css({"left":fx,"top":fy});  
 
   },50)
